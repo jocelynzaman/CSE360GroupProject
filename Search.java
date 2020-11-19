fc54fa61883e8097b46fc2e56d90be5fb36eff63
jddewaar <72539707+jddewaar@users.noreply.github.com>
2020-11-18 19:30:40 -0700

    Add files via upload

--- Search.java
+++ Search.java
@@ -2,18 +2,16 @@ import javax.swing.JButton;
 import javax.swing.JFileChooser;
 
 public class Search{
-	public static boolean search() {
+	public static String search() {
 		JButton button_OPEN = new JButton();
 		JFileChooser file = new JFileChooser();
 		file.setCurrentDirectory(new java.io.File("."));
 		file.setDialogTitle("File Explorer");
 		file.setFileSelectionMode(JFileChooser.FILES_ONLY);
 		if (file.showOpenDialog(button_OPEN) == JFileChooser.APPROVE_OPTION) {
-			//include code here;
-			return true;
+			return file.getSelectedFile().getAbsolutePath();
 		} else {
-			//include code here;
-			return false;
+			return "FILE_NOT_OPEN";
 		}
 		
 	}
