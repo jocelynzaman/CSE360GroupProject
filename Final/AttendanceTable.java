d037c4a04e8de0e02881d6bddf81ef2d08b01024
Jocelyn Zaman <44142922+jocelynzaman@users.noreply.github.com>
2020-11-23 23:13:39 -0700

    Added updateTableData and restructured JTable
    - updateTableData will add the column for attendance to the roster list
    - JTable is part of the DefaultTableModel to add columns to JTable a lot easier

--- Final/AttendanceTable.java
+++ Final/AttendanceTable.java
@@ -1,27 +1,35 @@
-package net.javacode.swing;
+// package net.javacode.swing;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Observable;
+import java.util.Observer;
 
-
-public class AttendanceTable {
+public class AttendanceTable{
+    DefaultTableModel tableModel;
     JTable attendanceTable;
     String[][] dataCollected;
     JScrollPane paneGUI;
+    ArrayList<String> dynamicColumnHeader;
+    ArrayList<ArrayList<String>> dynamicdataCollected;
+    Roster loadARoster;
 
 
     public AttendanceTable(){
         // prepareGUI();
+        attendanceTable = new JTable();
     }
 
 
     public JScrollPane prepareGUI(){
-
-        dataCollected = setTableData();
+        // dynamicColumnHeader = setTableHeader();
+        // dataCollected = setTableData();
 
         if (dataCollected.length > 0) {
-            attendanceTable = new JTable(dataCollected, setTableHeader().toArray());  
             paneGUI = new JScrollPane(attendanceTable);
             attendanceTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
             paneGUI.setSize(300, 300);
@@ -32,8 +40,17 @@ public class AttendanceTable {
         return null;
     }
 
+    public void setTableRoster()
+    {
+        dynamicColumnHeader = setTableHeader();
+        dataCollected = setTableData();
+        tableModel = new DefaultTableModel(dataCollected, dynamicColumnHeader.toArray());
+        attendanceTable.setModel(tableModel);
+
+    }
+
     private ArrayList<String> setTableHeader(){
-        ArrayList<String> dynamicColumnHeader = new ArrayList<String>();
+        dynamicColumnHeader = new ArrayList<String>();
         String[] initialHeaders = {"ID", "First Name", "Last Name", "Program", "Level", "ASURITE"};
         dynamicColumnHeader.addAll(Arrays.asList(initialHeaders));
         return dynamicColumnHeader;
@@ -43,12 +60,29 @@ public class AttendanceTable {
         Search searchFile = new Search();
         String fileName = searchFile.search();
         CSVReader readFile = new CSVReader();
-        Roster loadARoster = new Roster(readFile);
-        ArrayList<ArrayList<String>> dynamicdataCollected = loadARoster.fill(fileName);
-        String [][] dataCollected = dynamicdataCollected.stream().map(l -> l.stream().toArray(String[]::new)).toArray(String[][]::new);
+        loadARoster = new Roster(readFile);
+        dynamicdataCollected = loadARoster.fill(fileName);
+        dataCollected = dynamicdataCollected.stream().map(l -> l.stream().toArray(String[]::new)).toArray(String[][]::new);
         System.out.println(dynamicdataCollected.size());
 
         return dataCollected;
     }
 
+
+    public void updateTableData(int month, int day)
+    {
+        Search searchFile = new Search();
+        String fileName = searchFile.search();
+        CSVReader readFile = new CSVReader();
+        AttendenceList attendance = new AttendenceList(loadARoster, loadARoster.getSize(), readFile);
+        attendance.addAttendence(month, day, fileName);
+        String header = attendance.get(0).convertMonth(month+1) + " " + day;
+        Object columnData[] = attendance.get(0).getData();
+        tableModel.addColumn(header, columnData);
+    }
+
+    // public void update(Observable o, int month, int day)
+    // {
+    //     // dynamicdataCollected.add(((AttendenceList)o).addAttendence(month, day, String fileName));
+    // }
 }
