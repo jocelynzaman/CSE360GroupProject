452174586d87df61ec550d60cd6bcd40b51198d7
jddewaar <72539707+jddewaar@users.noreply.github.com>
2020-11-27 18:37:40 -0700

    Add files via upload

--- Final/MenuOptionListener.java
+++ Final/MenuOptionListener.java
@@ -15,29 +15,28 @@ import java.io.IOException;
 
 // import java.util.Properties;
 
-class MenuOptionListener implements MenuListener, ActionListener{
+class MenuOptionListener implements MenuListener, ActionListener {
     About aboutDialog = new About();
     MainMenu mainView;
     JMenuItem loadARoasterItem, addAttendanceItem, saveItem, plotDataItem;
     JScrollPane tableGUI;
     AttendanceTable attendanceTable = new AttendanceTable();
     PlotData plotData = new PlotData();
-    
-    //Date Picker
+
+    // Date Picker
     JDialog dateDialog = new JDialog();
     JPanel datePanel = new JPanel();
     JPanel DatePanel = new JPanel();
     DatePicker picker = new JDatePicker();
     JButton dateButton = new JButton("Confirm");
 
-    //flag to check if roster has been loaded
+    // flag to check if roster has been loaded
     boolean rosterLoaded = false;
 
-    //Error Dialog box
+    // Error Dialog box
     JOptionPane errorDialog = new JOptionPane();
 
-    public MenuOptionListener(MainMenu menu, JMenuItem load, JMenuItem add, JMenuItem save, JMenuItem plot)
-    {
+    public MenuOptionListener(MainMenu menu, JMenuItem load, JMenuItem add, JMenuItem save, JMenuItem plot) {
         mainView = menu;
         loadARoasterItem = load;
         addAttendanceItem = add;
@@ -48,50 +47,49 @@ class MenuOptionListener implements MenuListener, ActionListener{
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
 
-        if (actionEvent.getSource() == loadARoasterItem){
+        if (actionEvent.getSource() == loadARoasterItem) {
             try {
                 mainView.remove(tableGUI);
-                plotData.clearDataset(); //need to clear plot if new roster is loaded
+                mainView.repaint();
+                mainView.revalidate();
+                plotData.clearDataset(); // need to clear plot if new roster is loaded
             } catch (NullPointerException e) {
-                //There is no tableGUI in mainView.
+                // There is no tableGUI in mainView.
             }
             try {
                 attendanceTable.setTableRoster();
                 tableGUI = attendanceTable.prepareGUI();
                 rosterLoaded = true;
-                mainView.add(tableGUI); //prepareGUI returns NULL if user exits menu, does not cause error as far as I can see
-            } catch(NullPointerException pipipupu) {
+                mainView.add(tableGUI); // prepareGUI returns NULL if user exits menu, does not cause error as far as I
+                                        // can see
+            } catch (NullPointerException pipipupu) {
                 pipipupu.printStackTrace();
                 rosterLoaded = false;
             }
         }
-        if (actionEvent.getSource() == addAttendanceItem){
-            try {
-                mainView.remove(tableGUI);
-            } catch (NullPointerException e) {
-                //There is no tableGUI in mainView.
-                System.out.println("The roster has not been loaded yet.");
-            }
-            if (rosterLoaded)
-            {
-                //Show Date Picker
+        if (actionEvent.getSource() == addAttendanceItem) {
+            
+            if (rosterLoaded) {
+                // Show Date Picker
                 picker.setTextEditable(true);
                 picker.setShowYearButtons(true);
-                datePanel.add((JComponent)picker);
-                datePanel.setSize(100,100);
-                dateButton.setSize(50,50);
+                datePanel.add((JComponent) picker);
+                datePanel.setSize(100, 100);
+                dateButton.setSize(50, 50);
 
                 DatePanel.setLayout(new BorderLayout());
                 DatePanel.add(datePanel);
-                dateDialog.setLayout(new GridLayout(0,1));
-                dateDialog.setSize(300,100);
+                dateDialog.setLayout(new GridLayout(0, 1));
+                dateDialog.setSize(300, 100);
                 dateDialog.add(datePanel);
                 dateDialog.add(dateButton);
                 dateDialog.setVisible(true);
 
+                
+                MainMenu.fileMenu.repaint();
+
                 dateButton.addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent a)
-                        {
+                    public void actionPerformed(ActionEvent a) {
                         if (picker.getModel().isSelected()) {
                             picker.getModel().setSelected(false);
                             try {
@@ -104,39 +102,36 @@ class MenuOptionListener implements MenuListener, ActionListener{
                             dateDialog.setVisible(false);
                             tableGUI = attendanceTable.prepareGUI();
                             mainView.add(tableGUI);
-                            mainView.validate();
-                            mainView.repaint();
                         }
-                        }
+                    }
                 });
-            }
-            else
-            {
-                errorDialog.showMessageDialog(new JFrame(), "Need to load a roster first", "Error", JOptionPane.ERROR_MESSAGE);
+            } else {
+                errorDialog.showMessageDialog(new JFrame(), "Need to load a roster first", "Error",
+                        JOptionPane.ERROR_MESSAGE);
             }
         }
-        if (actionEvent.getSource() == saveItem){
+        if (actionEvent.getSource() == saveItem) {
             try {
                 attendanceTable.exportTable();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        if (actionEvent.getSource() == plotDataItem){
+        if (actionEvent.getSource() == plotDataItem) {
             plotData.prepareGUI();
         }
-
 
     }
 
     @Override
     public void menuSelected(MenuEvent menuEvent) {
-        JMenu menuSelected = (JMenu) menuEvent.getSource();
+        JMenu menuSelected = (JMenu) menuEvent.getSource();        
+
         menuSelected.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent mouseEvent) {
                 JMenu menuClicked = (JMenu) mouseEvent.getSource();
-                if(menuClicked.getText().equals("About")){
+                if (menuClicked.getText().equals("About")) {
                     aboutDialog.prepareGUI();
                 }
 
@@ -167,14 +162,12 @@ class MenuOptionListener implements MenuListener, ActionListener{
 
     @Override
     public void menuDeselected(MenuEvent menuEvent) {
-
-
+        //System.out.println("Menu Deselected");
     }
 
     @Override
     public void menuCanceled(MenuEvent menuEvent) {
-
+        //System.out.println("Menu Canceled");
     }
-
 
 }
