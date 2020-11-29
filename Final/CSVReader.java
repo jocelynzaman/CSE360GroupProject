415b0a6b9bfa2e6371f5a3cfa286c16897641c67
Thao Vu <hoaithao1710@gmail.com>
2020-11-28 23:07:24 -0700

    update JavaDoc comments

--- Final/CSVReader.java
+++ Final/CSVReader.java
@@ -1,13 +1,27 @@
+/**
+ * Author: Thao Vu
+ * SID: 127349445
+ */
 package net.javacode.swing;
 
 
 import java.io.*;
 import java.util.ArrayList;
 
+/**
+ * This class read a .csv file and generate a dynamic 2D array for its content
+ */
+
 public class CSVReader {
 
     public final String delimiter = ",";
 
+    /**
+     * This method always returns a dynamic 2D array of the file'scontent: rows as number of lines,
+     * and columns as the number of fields separated by commas
+     * @param  csvFile the name of the csv file needs to be read
+     * @return  a dynamic 2D array of the data being fetch with each line in the .csv file is an ArrayList
+     */
     public ArrayList<ArrayList<String>> read(String csvFile) {
         ArrayList<ArrayList<String>> generatedTable = new ArrayList<ArrayList<String>>();
         try {
@@ -28,13 +42,6 @@ public class CSVReader {
             br.close();
         } catch(IOException ioe) {
             ioe.printStackTrace();
-        }
-
-        for (int i = 0; i < generatedTable.size(); i++) {
-            for (int j = 0; j < generatedTable.get(i).size(); j++) {
-                System.out.print(generatedTable.get(i).get(j) + " ");
-            }
-            System.out.println();
         }
 
         return generatedTable;
